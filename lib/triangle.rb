c2f0ce3170ee6133538b2e38649bcdb7f7466b53
Chris Baum <christopher.baum@valpo.edu>
2021-02-10 00:01:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,16 +3,19 @@ require 'pry'
 class Triangle
   attr_reader :type
 
+  @side_lengths = []
+
   def initialize(side_1, side_2, side_3)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
+    @side_lengths << side_1
+    @side_lengths << side_2
+    @side_lengths << side_3
   end
 
   def kind
     if side_1 == side_2 && side_2 ==side_3
       @type = "equilateral"
     elsif
+      
 
   end
 end
